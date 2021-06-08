1b72d8edf773cc11cd04451649d71ebe4266a632
hongphongadm <hongphong.adm@gmail.com>
2021-06-08 21:55:54 +0700

    change 01

--- eShopSolution.WebApp/Startup.cs
+++ eShopSolution.WebApp/Startup.cs
@@ -45,7 +45,7 @@ namespace eShopSolution.WebApp
             app.UseRouting();
 
             app.UseAuthorization();
-
+            //srart here
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
